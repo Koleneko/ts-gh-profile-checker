1697401c25f1ea5e5472c70253ba5853d04cd5f3
Koleneko <vkoleneko@gmail.com>
2022-01-22 14:56:51 +0300

    Revert "Выполнил дз"
    This reverts commit cb986970dbbd15d25ab5ae0773db5364e85e2901.

--- src/components/Form/Form.props.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import { ChangeHandler, SubmitHandler, UseFormReturn } from "react-hook-form";
-
-export interface FormProps<TFormValues> {
-  className?: string;
-  onSubmit: SubmitHandler<TFormValues>;
-  onChange?: ChangeHandler;
-  children: (methods: UseFormReturn<TFormValues>) => ReactNode;
-}
--- src/components/elements/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React, { forwardRef } from "react";
-import { InputProps } from "../../types/Form.types";
-
-const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => (
-  <input ref={ref} {...props} />
-));
-
-export default Input;
